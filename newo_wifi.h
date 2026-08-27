45002e7afe07240bb8e4c0cadbe1458fb2c839aa
re:i shu <166681934+reitaard@users.noreply.github.com>
2026-08-27 16:19:13 +0700

    feat: add Newo Wi-Fi manager interface

---
+++ newo_wifi.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+#include "newo_storage.h"
+
+class NewoWiFi {
+ public:
+  explicit NewoWiFi(NewoStorage& storage);
+
+  void begin();
+  void loop();
+  void reloadSavedNetworks();
+
+  bool connected() const;
+  bool setupApActive() const;
+  String connectedSsid() const;
+  IPAddress localIP() const;
+  IPAddress setupIP() const;
+  int32_t rssi() const;
+
+ private:
+  bool tryConnect(uint32_t timeoutMs);
+  void startSetupAP();
+  void ensureMdns();
+  void stopMdns();
+
+  NewoStorage& storage_;
+  WiFiMulti wifiMulti_;
+  bool setupApActive_ = false;
+  bool mdnsStarted_ = false;
+  uint32_t lastReconnectAttemptMs_ = 0;
+};
