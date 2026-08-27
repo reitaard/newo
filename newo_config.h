9f12b006aaf0c1d74b520d7835415593bb4677fa
re:i shu <166681934+reitaard@users.noreply.github.com>
2026-08-27 16:18:38 +0700

    feat: add Newo device configuration

---
+++ newo_config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Arduino.h>
+
+namespace NewoConfig {
+
+constexpr char DEVICE_NAME[] = "Newo";
+constexpr char MDNS_HOST[] = "newo";
+constexpr char SETUP_AP_SSID[] = "Newo-Setup";
+
+constexpr uint8_t RGB_LED_PIN = 48;
+constexpr size_t MAX_SAVED_NETWORKS = 8;
+
+constexpr uint32_t INITIAL_CONNECT_TIMEOUT_MS = 15000;
+constexpr uint32_t RECONNECT_INTERVAL_MS = 10000;
+constexpr uint32_t RECONNECT_TIMEOUT_MS = 5000;
+constexpr uint32_t PORTAL_REBOOT_DELAY_MS = 1500;
+
+}  // namespace NewoConfig
