2eea0a41569c4933d6c60e411ea18349b7104ca1
re:i shu <166681934+reitaard@users.noreply.github.com>
2026-09-18 01:14:20 +0700

    Remove Wall-E wake fallback

--- Newo/newo_wake_backend_select.h
+++ Newo/newo_wake_backend_select.h
@@ -1,16 +1,7 @@
 #pragma once
 
-// Alfred is the physical-test default on this branch. Set this to 0 (or pass
-// -DNEWO_USE_ALFRED_MWW=0) to compile the existing ESP-SR / Hi Wall-E backend
-// instead without changing NewoAudio's ownership/session logic.
-#ifndef NEWO_USE_ALFRED_MWW
-#define NEWO_USE_ALFRED_MWW 1
-#endif
-
-#if NEWO_USE_ALFRED_MWW
 #include "newo_mww_engine.h"
+
+// Alfred is the only local wake backend. The former ESP-SR / Hi Wall-E
+// fallback has been removed from the firmware.
 using NewoActiveWakeEngine = NewoMicroWakeEngine;
-#else
-#include "newo_wake_engine.h"
-using NewoActiveWakeEngine = NewoEspSrWakeEngine;
-#endif
