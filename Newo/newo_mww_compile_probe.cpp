f803d45d0adfd70653769b7258c7cddd2ddcaac5
re:i shu <166681934+reitaard@users.noreply.github.com>
2026-09-17 23:24:47 +0700

    Probe TFLM microfrontend for Alfred

--- Newo/newo_mww_compile_probe.cpp
+++ Newo/newo_mww_compile_probe.cpp
@@ -1,7 +1,8 @@
 // Temporary compile probe for the Alfred microWakeWord integration.
 // This file deliberately does not change runtime behaviour. It proves that the
-// Arduino-ESP32 3.3.10 toolchain exposes the TFLite Micro APIs required by the
-// streaming Alfred model before we touch the working ESP-SR wake path.
+// Arduino-ESP32 3.3.10 toolchain exposes the TFLite Micro + microfrontend APIs
+// required by the streaming Alfred model before we touch the working ESP-SR
+// wake path.
 
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/micro/micro_allocator.h"
@@ -9,6 +10,11 @@
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 #include "tensorflow/lite/micro/micro_resource_variable.h"
 #include "tensorflow/lite/schema/schema_generated.h"
+
+extern "C" {
+#include "tensorflow/lite/experimental/microfrontend/lib/frontend.h"
+#include "tensorflow/lite/experimental/microfrontend/lib/frontend_util.h"
+}
 
 namespace {
 using NewoMwwProbeResolver = tflite::MicroMutableOpResolver<14>;
@@ -34,9 +40,13 @@ void newoMwwCompileProbe() {
   tflite::MicroResourceVariables* resources = nullptr;
   tflite::MicroInterpreter* interpreter = nullptr;
   const tflite::Model* model = nullptr;
+  FrontendConfig frontendConfig{};
+  FrontendState frontendState{};
   (void)allocator;
   (void)resources;
   (void)interpreter;
   (void)model;
+  (void)frontendConfig;
+  (void)frontendState;
 }
 }  // namespace
